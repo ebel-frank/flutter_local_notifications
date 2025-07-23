756af950f3ab9a04b7d84e56189add85564ea7c8
Frank C. Ebeledike <frankebeledike@gmail.com>
2025-07-23 22:07:20 +0100

    hotfix

--- flutter_local_notifications/android/src/main/java/com/dexterous/flutterlocalnotifications/FlutterLocalNotificationsPlugin.java
+++ flutter_local_notifications/android/src/main/java/com/dexterous/flutterlocalnotifications/FlutterLocalNotificationsPlugin.java
@@ -1401,6 +1401,49 @@ public class FlutterLocalNotificationsPlugin
     return null;
   }
 
+  // private static String getNextFireDateMatchingDateTimeComponents(
+  //     NotificationDetails notificationDetails) {
+  //   ZoneId zoneId = ZoneId.of(notificationDetails.timeZoneName);
+  //   ZonedDateTime scheduledDateTime =
+  //       ZonedDateTime.of(LocalDateTime.parse(notificationDetails.scheduledDateTime), zoneId);
+  //   ZonedDateTime now = ZonedDateTime.now(zoneId);
+  //   ZonedDateTime nextFireDate =
+  //       ZonedDateTime.of(
+  //           now.getYear(),
+  //           now.getMonthValue(),
+  //           now.getDayOfMonth(),
+  //           scheduledDateTime.getHour(),
+  //           scheduledDateTime.getMinute(),
+  //           scheduledDateTime.getSecond(),
+  //           scheduledDateTime.getNano(),
+  //           zoneId);
+  //   while (nextFireDate.isBefore(now)) {
+  //     // adjust to be a date in the future that matches the time
+  //     nextFireDate = nextFireDate.plusDays(1);
+  //   }
+  //   if (notificationDetails.matchDateTimeComponents == DateTimeComponents.Time) {
+  //     return DateTimeFormatter.ISO_LOCAL_DATE_TIME.format(nextFireDate);
+  //   } else if (notificationDetails.matchDateTimeComponents == DateTimeComponents.DayOfWeekAndTime) {
+  //     while (nextFireDate.getDayOfWeek() != scheduledDateTime.getDayOfWeek()) {
+  //       nextFireDate = nextFireDate.plusDays(1);
+  //     }
+  //     return DateTimeFormatter.ISO_LOCAL_DATE_TIME.format(nextFireDate);
+  //   } else if (notificationDetails.matchDateTimeComponents
+  //       == DateTimeComponents.DayOfMonthAndTime) {
+  //     while (nextFireDate.getDayOfMonth() != scheduledDateTime.getDayOfMonth()) {
+  //       nextFireDate = nextFireDate.plusDays(1);
+  //     }
+  //     return DateTimeFormatter.ISO_LOCAL_DATE_TIME.format(nextFireDate);
+  //   } else if (notificationDetails.matchDateTimeComponents == DateTimeComponents.DateAndTime) {
+  //     while (nextFireDate.getMonthValue() != scheduledDateTime.getMonthValue()
+  //         || nextFireDate.getDayOfMonth() != scheduledDateTime.getDayOfMonth()) {
+  //       nextFireDate = nextFireDate.plusDays(1);
+  //     }
+  //     return DateTimeFormatter.ISO_LOCAL_DATE_TIME.format(nextFireDate);
+  //   }
+  //   return null;
+  // }
+
   // Get the Next Valid Reminder time based on end date, start date, selected days of the week, and notification times.
   private static String getNextFireDateMatchingTime(NotificationDetails notificationDetails) {
     // Set up timezone
