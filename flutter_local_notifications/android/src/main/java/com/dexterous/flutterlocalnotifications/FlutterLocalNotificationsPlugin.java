c221811f50014b6467dfc8d00b09c1b837a1336b
Ifeoluwa Andrew Omole <iomole3@gmail.com>
2025-09-18 11:38:34 +0100

    Revert "Request notification permission"
    This reverts commit 58be18074da7c608085ea51be284d3dffce5f9e4.

--- flutter_local_notifications/android/src/main/java/com/dexterous/flutterlocalnotifications/FlutterLocalNotificationsPlugin.java
+++ flutter_local_notifications/android/src/main/java/com/dexterous/flutterlocalnotifications/FlutterLocalNotificationsPlugin.java
@@ -592,8 +592,6 @@ public class FlutterLocalNotificationsPlugin
         getBroadcastPendingIntent(context, notificationDetails.id, notificationIntent);
 
     AlarmManager alarmManager = getAlarmManager(context);
-    requestScheduleExactAlarmsPermission(context, alarmManager);
-
     setupAlarm(
         notificationDetails,
         alarmManager,
@@ -624,8 +622,6 @@ public class FlutterLocalNotificationsPlugin
             .toInstant()
             .toEpochMilli();
 
-
-    requestScheduleExactAlarmsPermission(context, alarmManager);
     setupAlarm(notificationDetails, alarmManager, epochMilli, pendingIntent);
 
     if (updateScheduledNotificationsCache) {
@@ -796,14 +792,6 @@ public class FlutterLocalNotificationsPlugin
   private static void checkCanScheduleExactAlarms(AlarmManager alarmManager) {
     if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S && !alarmManager.canScheduleExactAlarms()) {
       throw new ExactAlarmPermissionException();
-    }
-  }
-
-  private static void requestScheduleExactAlarmsPermission(Context context, AlarmManager alarmManager) {
-    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S && !alarmManager.canScheduleExactAlarms()) {
-      Intent intent = new Intent(Settings.ACTION_REQUEST_SCHEDULE_EXACT_ALARM);
-      intent.setData(Uri.parse("package:" + context.getPackageName()));
-      context.startActivity(intent);
     }
   }
 
@@ -1800,21 +1788,17 @@ public class FlutterLocalNotificationsPlugin
   private void zonedSchedule(MethodCall call, Result result) {
     NotificationDetails notificationDetails = extractNotificationDetails(result, call.arguments());
     if (notificationDetails != null) {
-//      if (notificationDetails.matchDateTimeComponents != null) {
-//        notificationDetails.scheduledDateTime =
-//            getNextFireDateMatchingTime(notificationDetails);
-//            Log.d(TAG, notificationDetails.notificationTimes + " " + notificationDetails.scheduledDateTime + " " + notificationDetails.scheduledEndDateTime + " " + notificationDetails.daysOfTheWeek.toString());
-//      } else {
-//          result.error("888","No match date time components",null);
-//      }
+      if (notificationDetails.matchDateTimeComponents != null) {
+        notificationDetails.scheduledDateTime =
+            getNextFireDateMatchingTime(notificationDetails);
+            Log.d(TAG, notificationDetails.notificationTimes + " " + notificationDetails.scheduledDateTime + " " + notificationDetails.scheduledEndDateTime + " " + notificationDetails.daysOfTheWeek.toString());
+      }
       try {
         zonedScheduleNotification(applicationContext, notificationDetails, true);
         result.success(null);
       } catch (PluginException e) {
         result.error(e.code, e.getMessage(), null);
       }
-    } else {
-        result.error("777", "Invalid icon, big picture resource, raw sound resource, or details", null);
     }
   }
 
