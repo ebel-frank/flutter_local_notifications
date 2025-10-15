940a4fb793c083c33194cc7f604650ca446901c0
Ifeoluwa Andrew Omole <iomole3@gmail.com>
2025-10-15 14:17:24 +0100

    Use foreground service for reliable notifications

--- flutter_local_notifications/android/src/main/java/com/dexterous/flutterlocalnotifications/ScheduledNotificationReceiver.java
+++ flutter_local_notifications/android/src/main/java/com/dexterous/flutterlocalnotifications/ScheduledNotificationReceiver.java
@@ -1,22 +1,19 @@
 package com.dexterous.flutterlocalnotifications;
 
-import android.app.Notification;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
-import android.util.Log;
+import android.os.Build;
 
 import androidx.annotation.Keep;
-import androidx.core.app.NotificationManagerCompat;
 
 import com.dexterous.flutterlocalnotifications.models.NotificationDetails;
-import com.dexterous.flutterlocalnotifications.utils.StringUtils;
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
 
 import java.lang.reflect.Type;
 
-/** Created by michaelbui on 24/3/18. */
+/** Created by michaelbui on 24/3/18. Edited by andy-ife on 15/10/25 */
 @Keep
 public class ScheduledNotificationReceiver extends BroadcastReceiver {
 
@@ -26,41 +23,26 @@ public class ScheduledNotificationReceiver extends BroadcastReceiver {
   @SuppressWarnings("deprecation")
   public void onReceive(final Context context, Intent intent) {
     String notificationDetailsJson =
-        intent.getStringExtra(FlutterLocalNotificationsPlugin.NOTIFICATION_DETAILS);
-    if (StringUtils.isNullOrEmpty(notificationDetailsJson)) {
-      // This logic is needed for apps that used the plugin prior to 0.3.4
+            intent.getStringExtra(FlutterLocalNotificationsPlugin.NOTIFICATION_DETAILS);
 
-      Notification notification;
-      int notificationId = intent.getIntExtra("notification_id", 0);
+    Gson gson = FlutterLocalNotificationsPlugin.buildGson();
+    Type type = new TypeToken<NotificationDetails>() {}.getType();
+    NotificationDetails notificationDetails = gson.fromJson(notificationDetailsJson, type);
 
-      if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.TIRAMISU) {
-        notification = intent.getParcelableExtra("notification", Notification.class);
-      } else {
-        notification = intent.getParcelableExtra("notification");
-      }
+    Intent serviceIntent = new Intent(context, ForegroundService.class);
 
-      if (notification == null) {
-        // This means the notification is corrupt
-        FlutterLocalNotificationsPlugin.removeNotificationFromCache(context, notificationId);
-        Log.e(TAG, "Failed to parse a notification from  Intent. ID: " + notificationId);
-        return;
-      }
+    serviceIntent.putExtra(
+            ForegroundServiceStartParameter.EXTRA,
+            new ForegroundServiceStartParameter(
+                    notificationDetails,
+                    ForegroundService.START_STICKY_COMPATIBILITY,
+                    null)
+    );
 
-      notification.when = System.currentTimeMillis();
-      NotificationManagerCompat notificationManager = NotificationManagerCompat.from(context);
-      notificationManager.notify(notificationId, notification);
-      boolean repeat = intent.getBooleanExtra("repeat", false);
-      if (!repeat) {
-        FlutterLocalNotificationsPlugin.removeNotificationFromCache(context, notificationId);
-      }
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
+      context.startForegroundService(serviceIntent);
     } else {
-      Gson gson = FlutterLocalNotificationsPlugin.buildGson();
-      Type type = new TypeToken<NotificationDetails>() {}.getType();
-      NotificationDetails notificationDetails = gson.fromJson(notificationDetailsJson, type);
-
-      Log.d("From Andy","Alarm Broadcast Fired!");
-      FlutterLocalNotificationsPlugin.showNotification(context, notificationDetails);
-      FlutterLocalNotificationsPlugin.scheduleNextNotification(context, notificationDetails);
+      context.startService(serviceIntent);
     }
   }
 }
